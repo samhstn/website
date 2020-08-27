193e5f00999b114378cd608d2a3e9bf42bf07899
samhstn <shouston3@me.com>
2020-08-27 17:09:52 +0100

    remove old deploy.sh and use separated out deploy-infra.sh

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -26,120 +26,13 @@ if [[ -z "$AWS_ROOT_ACCOUNT_ID" ]]; then
   echo "AWS_ROOT_ACCOUNT_ID=$AWS_ROOT_ACCOUNT_ID" >> $ENV_FILE
 fi
 
-printf 'Deploying root iam.yml '
-aws cloudformation deploy \
-  --profile samhstn-root \
-  --stack-name samhstn \
-  --template-file ./infra/root/iam.yml \
-  --no-fail-on-empty-changeset \
-  --capabilities CAPABILITY_NAMED_IAM \
-  --parameter-overrides \
-    Project=Samhstn \
-    AccountId=$AWS_ADMIN_ACCOUNT_ID | tr '\n' ' ' | sed 's/^ //' | sed 's/  / /g'
-
-printf 'Deploying root setup.yml '
-aws cloudformation deploy \
-  --profile samhstn-root \
-  --stack-name samhstn-setup \
-  --template-file ./infra/root/setup.yml \
-  --no-fail-on-empty-changeset \
-  --capabilities CAPABILITY_NAMED_IAM \
-  --parameter-overrides \
-    EmailBucket=samhstn-mail-$AWS_ROOT_ACCOUNT_ID \
-    CloudformationBucket=samhstn-cfn-$AWS_ROOT_ACCOUNT_ID | tr '\n' ' ' | sed 's/^ //' | sed 's/  / /g'
-
-mkdir -p infra/cfn_output/root
-
-echo 'Packaging root main.yml'
-PACKAGE_ERR="$(aws cloudformation package \
-  --profile samhstn-root \
-  --template ./infra/root/main.yml \
-  --s3-bucket samhstn-cfn-$AWS_ROOT_ACCOUNT_ID \
-  --output-template-file infra/cfn_output/root/main.yml 2>&1)"
-
-if ! [[ $PACKAGE_ERR =~ "Successfully packaged artifacts" ]]; then
-  echo "ERROR while running 'aws cloudformation package' command:"
-  echo $PACKAGE_ERR
-  exit 1
-fi
-
-printf 'Deploying root main.yml '
-aws cloudformation deploy \
-  --profile samhstn-root \
-  --stack-name samhstn-main \
-  --template-file ./infra/cfn_output/root/main.yml \
-  --no-fail-on-empty-changeset \
-  --capabilities CAPABILITY_NAMED_IAM \
-  --parameter-overrides \
-    FromEmail=hello@samhstn.com \
-    NotificationEmail=$SAMHSTN_NOTIFICATION_EMAIL | tr '\n' ' ' | sed 's/^ //' | sed 's/  / /g'
-
-if [[ -z $(aws ses --profile samhstn-root describe-active-receipt-rule-set) ]]; then
-  echo "setting rule set"
-  aws ses --profile samhstn-root set-active-receipt-rule-set --rule-set-name SamhstnRuleSet
-fi
+./infra/root/deploy.sh
 
 if [[ -z $TEMP_PASSWORD ]]; then
   TEMP_PASSWORD=$(node -e "console.log(Math.random().toString(36).slice(2))")
   echo "TEMP_PASSWORD=$TEMP_PASSWORD" >> $ENV_FILE
 fi
 
-aws cloudformation deploy \
-  --profile samhstn-admin \
-  --stack-name project-iam \
-  --template-file ./infra/samhstn/project-iam.yml \
-  --no-fail-on-empty-changeset \
-  --capabilities CAPABILITY_NAMED_IAM \
-  --parameter-overrides \
-    TempPassword=$TEMP_PASSWORD \
-    RootAccountId=$AWS_ROOT_ACCOUNT_ID | tr '\n' ' ' | sed 's/^ //' | sed 's/  / /g'
+./infra/samhstn/deploy.sh
 
-aws cloudformation deploy \
-  --profile samhstn-admin \
-  --stack-name setup \
-  --template-file ./infra/samhstn/setup.yml \
-  --no-fail-on-empty-changeset \
-  --parameter-overrides \
-    CloudformationBucket=samhstn-cfn-$AWS_ADMIN_ACCOUNT_ID | tr '\n' ' ' | sed 's/^ //' | sed 's/  / /g'
-
-mkdir -p infra/cfn_output/samhstn
-
-PACKAGE_ERR="$(aws cloudformation package \
-  --profile samhstn-admin \
-  --template ./infra/samhstn/main.yml \
-  --s3-bucket samhstn-cfn-$AWS_ADMIN_ACCOUNT_ID \
-  --output-template-file infra/cfn_output/samhstn/main.yml 2>&1)"
-
-if ! [[ $PACKAGE_ERR =~ "Successfully packaged artifacts" ]]; then
-  echo "ERROR while running 'aws cloudformation package' command:"
-  echo $PACKAGE_ERR
-  exit 1
-fi
-
-aws cloudformation deploy \
-  --profile samhstn-admin \
-  --stack-name main \
-  --template-file ./infra/cfn_output/samhstn/main.yml \
-  --no-fail-on-empty-changeset \
-  --capabilities CAPABILITY_NAMED_IAM \
-  --parameter-overrides \
-    GithubPAToken=$SAMHSTN_PA_TOKEN \
-    GithubMasterBranch=$GITHUB_MASTER_BRANCH | tr '\n' ' ' | sed 's/^ //' | sed 's/  / /g'
-
-WEBHOOK_URL=$(aws cloudformation list-exports --profile samhstn-admin | jp -u "Exports[?Name=='WebhookEndpoint'].Value|[0]")
-GITHUB_SECRET=$(aws secretsmanager get-secret-value --secret-id /GithubSecret --query SecretString --output text)
-
-if ! [ -d venv ]; then
-  echo "creating new venv"
-  python3 -m venv venv
-  venv/bin/python3 -m pip install --upgrade pip
-fi
-
-if ! [[ $(venv/bin/pip list) =~ "requests" ]]; then
-  venv/bin/pip install requests
-fi
-
-export WEBHOOK_URL
-export GITHUB_SECRET
-
-venv/bin/python infra/configure_github_webhook.py
+./infra/samhstn/configure_github_webhook.sh
