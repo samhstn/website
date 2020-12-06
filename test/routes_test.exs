66a38fe5060c139fdefa0353cb7a0e9001af8f3f
samhstn <shouston3@me.com>
2020-12-06 18:38:38 +0000

    add test for state update when calling Routes.get/1 #107

--- test/routes_test.exs
+++ test/routes_test.exs
@@ -1,8 +1,8 @@
-defmodule SamhstnWeb.RoutesTest do
+defmodule Samhstn.RoutesTest do
   use ExUnit.Case, async: true
 
   alias Samhstn.Routes
-  alias Samhstn.Routes.Route
+  alias Samhstn.Routes.{Route, RouteRef}
 
   describe "Samhstn.Routes" do
     setup do
@@ -13,6 +13,24 @@ defmodule SamhstnWeb.RoutesTest do
 
     test "get/1 returns data" do
       assert {:ok, %Route{body: body, path: "vimrc", type: :text}} = Routes.get("vimrc")
+      assert body =~ "syntax enable"
+    end
+
+    test "stores initial routes data as RouteRefs in state and adds Route data after calling get/1",
+         %{pid: pid} do
+      route_ref = %RouteRef{
+        path: "vimrc",
+        ref: "https://raw.githubusercontent.com/samhstn/my-config/master/.vimrc",
+        source: "url",
+        type: :text
+      }
+
+      assert {[^route_ref], []} = :sys.get_state(pid)
+      assert {:ok, _} = Routes.get("vimrc")
+
+      assert {[^route_ref], [%Route{body: body, path: "vimrc", type: :text}]} =
+               :sys.get_state(pid)
+
       assert body =~ "syntax enable"
     end
   end
